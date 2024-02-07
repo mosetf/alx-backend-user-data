c8dd1ca99927c6abff304ef3c6bcc6e9b66dd4d6
mosetf <stanlym315@gmail.com>
2024-02-07 23:29:55 +0300

    Add filtered_logger.py file

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Regex-ing
+"""
+
+
+def filter_datum():
+    """
+    This function filters the data to remove any sensitive information.
+    It ensures that any personal data is not logged or stored.
+    """
