949919ed6b743ce72fc1aab385f22f8b812ccd81
mosetf <stanlym315@gmail.com>
2024-02-08 22:35:36 +0300

    Add PII data filtering function to filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,10 +2,26 @@
 """
 Regex-ing
 """
+import os
+import re
+import logging
+import mysql.connector
+from typing import List
 
 
-def filter_datum():
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
     """
     This function filters the data to remove any sensitive information.
     It ensures that any personal data is not logged or stored.
     """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
