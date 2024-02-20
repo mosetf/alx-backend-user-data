fad633753454ba0861f81f7cb857f4a94fa57142
mosetf <stanlym315@gmail.com>
2024-02-20 22:37:53 +0300

    Add reset_token column to User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,3 +11,4 @@ class User(Base):
     email = column(String(250), nullable=False)
     hashed_password = column(String(250), nullable=False)
     session_id = column(String(250), nullable=True)
+    reset_token = column(String(250), nullable=True)
