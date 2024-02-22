ce25bf115e2793cb5d955203635a6ddc9d3efeb9
mosetf <stanlym315@gmail.com>
2024-02-22 23:25:58 +0300

    Add user authentication features to Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 """
-App module
+Simple Flask app with user authentication features.
 """
-from flask import Flask
+from flask import Flask, jsonify, request, abort, redirect
+
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
